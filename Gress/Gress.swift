f570c3951ce363cd68f5204d6d1c865839e6bf28
Umar Qattan <u.qattan@gmail.com>
2015-08-19 02:52:50 -0700

    Update: modified the UI by adding a new scene to the Profile Creation Process

---
+++ Gress/Gress.swift
@@ -0,0 +1,9 @@
+//
+//  Gress.swift
+//  Gress
+//
+//  Created by Umar Qattan on 8/18/15.
+//  Copyright (c) 2015 Umar Qattan. All rights reserved.
+//
+
+import Foundation
---
+++ Gress/NewGressProfileNavigationController.swift
@@ -0,0 +1,9 @@
+//
+//  NewGressProfileNavigationController.swift
+//  Gress
+//
+//  Created by Umar Qattan on 8/18/15.
+//  Copyright (c) 2015 Umar Qattan. All rights reserved.
+//
+
+import Foundation
