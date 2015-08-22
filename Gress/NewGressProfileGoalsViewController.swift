4d65ced3f96fe5568973c345b7a6370ecd8c2e8f
Umar Qattan <u.qattan@gmail.com>
2015-08-22 02:37:30 -0700

    Update: Added a new set of images to xcassets

---
+++ Gress/NewGressProfileGoalsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NewGressProfileGoalsViewController.swift
+//  Gress
+//
+//  Created by Umar Qattan on 8/21/15.
+//  Copyright (c) 2015 Umar Qattan. All rights reserved.
+//
+
+import Foundation
